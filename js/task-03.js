a90b374cc7e48d5e1ab6c6ca1b0eba3b011b8754
Yuliya Chukhil <yuliya.chukhil@gmail.com>
2021-04-07 19:40:36 +0300

    used template string & insertAdjacentHTML

---
+++ js/task-03.js
@@ -0,0 +1,30 @@
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const pictureGallery = document.getElementById("gallery");
+pictureGallery.style.listStyleType = "none";
+pictureGallery.style.display = "flex";
+pictureGallery.style.alignItems = "center";
+pictureGallery.style.justifyContent = "space-between";
+pictureGallery.style.border = "5px dashed teal";
+
+const imgElem = images.map((elem) => {
+  return `<li><img src="${elem.url}" alt="${elem.alt}" width="450"></li>`;
+});
+pictureGallery.insertAdjacentHTML("beforeend", imgElem.join(""));
+console.log(imgElem);
